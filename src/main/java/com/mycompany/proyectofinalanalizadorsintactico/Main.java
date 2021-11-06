24373976c752a1910ab1ee639ff1521542e497cc
Melanni Tzul <melannitzul@gmail.com>
2021-11-06 03:24:46 -0600

    Actualizaciones parte grafica

--- src/main/java/com/mycompany/proyectofinalanalizadorsintactico/Main.java
+++ src/main/java/com/mycompany/proyectofinalanalizadorsintactico/Main.java
@@ -1,13 +1,17 @@
 
 package com.mycompany.proyectofinalanalizadorsintactico;
 
+import com.mycompany.proyectofinalanalizadorsintactico.ventanaGrafica.VentanaPrincipal;
+
 /**
  *
  * @author melannitzul
  */
 public class Main {
     public static void main (String[] args ){
+    VentanaPrincipal ventana = new VentanaPrincipal();//Crear objeto de mi ventana principal    
+    ventana.setVisible(true);//hacer visible la ventana
     
-    
+   
     }
 }
